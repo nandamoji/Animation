f7be8c708f75eeaefe147b891e5570eaadb4605b
Nanda <nandamoji@gmail.com>
2019-05-17 14:24:45 +0700

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Dr. Nanda L Prasetya, MMSc (MedEd) on 5/17/19.
+//  Copyright © 2019 Dr. Nanda L Prasetya, MMSc (MedEd). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
